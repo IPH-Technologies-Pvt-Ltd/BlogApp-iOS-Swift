dd690e1b2f366267e5c5e55c514560c5160c8b2a
Smriti kaur <smriti.kaur@iphtechnologies.com>
2023-08-18 13:36:04 +0530

    initial Commit

---
+++ BlogHorizon/CoursesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CoursesViewController.swift
+//  BlogHorizon
+//
+//  Created by IPH Technologies Pvt. Ltd. on 23/05/23.
+//
+
+import UIKit
+class CoursesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
